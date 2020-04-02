e1c65198ea7f1b71397b2f9518cc25106f878cf5
Kiryl <kiryl.yushkevich@gmail.com>
2020-04-02 20:48:49 +0300

    feat(server): added analytics route branch

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/api', require('./api'));
 router.use('/data', require('./data'));
+router.use('/analytics', require('./analytics'));
 
 module.exports = router;
